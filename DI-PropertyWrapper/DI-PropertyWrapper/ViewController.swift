39c0a9bd6218254bbfc7b60603e877688334ba79
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2023-06-07 14:28:34 +0300

    initial commit

---
+++ DI-PropertyWrapper/DI-PropertyWrapper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI-PropertyWrapper
+//
+//  Created by Ahmed Fayek on 07/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
